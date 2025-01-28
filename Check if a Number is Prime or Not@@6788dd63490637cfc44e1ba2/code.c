cd5cd57f38d3926ed2a1c645b62a803a77f7ec70
agrimanand <agrimanand2006@gmail.com>
2025-01-28 17:31:57 +0530

    Solution to Check if a Number is Prime or Not

--- Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
+++ Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
@@ -3,7 +3,7 @@
 int main() {
     int a;
     scanf("%d",&a);
-    if(a%2 || a%3 || a%5 ||){
+    if(a%2==0 || a%3==0 || a%5==0 ){
         printf("Prime");
     }else{
         printf("Not prime");
