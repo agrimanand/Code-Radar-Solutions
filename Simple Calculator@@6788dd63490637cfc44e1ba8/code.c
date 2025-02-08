370ff3e25be78c9c8700977afb400427a1c8bc2d
agrimanand <agrimanand2006@gmail.com>
2025-02-09 03:17:41 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-int main() {
-    int a,b;
-    char op;
-    scanf("%d %d %c",&a &b &op);
-    int answer=0;
-    if(op=='+'){
-        answer = a+b;
-    }else if(op == '-'){
-        answer =a-b;
-    }else if(op=='%'){
-        answer = a%b;
-    }else if(op=='/'){
-        answer=a/b;
-    }else{
-        answer = "error";
-    }
-    
 
-    printf("%d",answer);
+int main() {
+    int a, b;
+    char op;
+    printf("Enter two integers and an operator (+, -, *, /): ");
+    scanf("%d %d %c", &a, &b, &op);
+    int answer;
+    if (op == '+') {
+        answer = a + b;
+    } else if (op == '-') {
+        answer = a - b;
+    } else if (op == '*') {
+        answer = a * b;
+    } else if (op == '/') {
+        if (b == 0) {
+            printf("Error: Division by zero\n");
+            return 1;
+        }
+        answer = a / b;
+    } else {
+        printf("Error: Invalid operator\n");
+        return 1;
+    }
+
+    printf("%d\n", answer);
     return 0;
 }
